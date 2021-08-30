3f8b2672b640e9ba526add99d081343df3537ab5
krist7599555 <krist7599555@gmail.com>
2021-08-30 11:20:37 +0700

    📖 DOC: add calculate comment

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use std::fs::File;
 use std::io::Write;
-use nalgebra::{Vector3, vector};
+use nalgebra::{Matrix, Vector3, vector};
 
 type Vec3 = Vector3<f32>;
 struct Ray {
@@ -28,11 +28,29 @@ struct Sphere {
 }
 impl RayHitable for Sphere {
     fn hit(&self, ray: &Ray) -> bool {
-        let oc = ray.origin - self.center;
-        let a = ray.direction.dot(&ray.direction);
-        let b = 2.0 * oc.dot(&ray.direction);
-        let c = oc.dot(&oc) - self.radius.powi(2);
-        let discriminant = b * b - 4.0 * a * c;
+        // (x-cx)^2 + (y-cy)^2 + (z-cz)^2 = r^2 (sphere equation)
+        // dot(a, a) = a.x^2 + a.y^2 + a.z^2
+        // so define fun dotself(x) = dot(x, x)
+        // hit sphere if FORSOME(t)
+        //   = dotself(ray(t) - sphere.center) = r^2
+        //   = dotself(ray.origin + t*ray.direction - sphere.center) = r^2
+        //   = dotself((t*ray.direction) + (ray.origin - sphere.center)) = r^2
+        //   = [t^2 * dotself(ray.direction)] + [2 * t * dot(ray.direction, ray.origin - sphere.center)] + [dotself(ray.origin - sphere.center) - (r^2)] = 0
+        //  where 
+        //        ray.origin = A
+        //        ray.direction = B
+        //        ray(t) = A + t * B,
+        //        sphere.center = C
+        //   = [t^2 * dotself(B)] + [2 * t * dot(B, A-C)] + [dotself(A-C) - (r^2)] = 0
+        //   = [t^2 * (a)] + [t * (b)] + [(c)] = 0
+        // just solve quadratic equation (-b +/- sqrt(b^2 - 4ac)) / 2a
+        // we need to check just is answer exists. so, check if (b^2 - 4ac) is positive
+        let sphere = self;
+        let center = ray.origin - sphere.center;
+        let quadratic_a = Matrix::dot(&ray.direction, &ray.direction);
+        let quadratic_b = Matrix::dot(&center,&ray.direction) * 2.0;
+        let quadratic_c = Matrix::dot(&center, &center) - sphere.radius.powi(2);
+        let discriminant = quadratic_b.powi(2) - 4.0 * quadratic_a * quadratic_c;
         discriminant > 0.0
     }
 }
