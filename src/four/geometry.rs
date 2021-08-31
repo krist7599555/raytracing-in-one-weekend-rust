b1eb5cc94898a285444ccb70cfd78cc79a8ba1bf
krist7599555 <krist7599555@gmail.com>
2021-08-31 13:26:23 +0700

    📖 DOC: sphere hit calculate

--- src/four/geometry.rs
+++ src/four/geometry.rs
@@ -17,43 +17,58 @@ pub struct Sphere {
   pub radius: f32
 }
 impl Geometry for Sphere {
+  /// `(x-cx)² + (y-cy)² + (z-cz)² = r²` (sphere equation)<br>
+  /// so define 
+  /// ```rust 
+  /// fn dotself(a) = dot(a, a) = a.x² + a.y² + a.z²
+  /// ```
+  /// ```rust
+  /// hit sphere if forsome(t)
+  ///     where 
+  ///       ray.origin = A
+  ///       ray.direction = B
+  ///       ray(t) = A + t * B
+  ///       sphere.center = C
+  ///   = dotself(ray(t) - C) = r²
+  ///   = dotself(A + t * B + t * ray.direction - C) = r²
+  ///   = dotself((t * B) + (A - C)) = r²
+  ///   = (t² * dotself(B)) 
+  ///     + (2 * t * dot(B, A - C)) 
+  ///     + (dotself(A - C) - (r²)) = 0
+  /// ```
+  /// just solve quadratic equation `(-b +/- sqrt(b² - 4ac)) / 2a`
+  /// we need to check just is answer exists. so, check if `(b² - 4ac)` is positive
+  ///
+  /// also optimize, we only need normal vector not care about scalar
+  /// ```rust
+  /// if B = 2b
+  ///  = -B +/- sqrt(B² - 4ac) / 2a
+  ///  = -2b +/- 2sqrt(b²-ac) / 2a
+  ///  = 2 (b +/- sqrt(b²-ac) / a)
+  ///    ^remove 2
+  /// ```
   fn hit(&self, ray: &Ray) -> Option<HitRecordGeometric> {
-      // (x-cx)^2 + (y-cy)^2 + (z-cz)^2 = r^2 (sphere equation)
-      // dot(a, a) = a.x^2 + a.y^2 + a.z^2
-      // so define fun dotself(x) = dot(x, x)
-      // hit sphere if FORSOME(t)
-      //   = dotself(ray(t) - sphere.center) = r^2
-      //   = dotself(ray.origin + t*ray.direction - sphere.center) = r^2
-      //   = dotself((t*ray.direction) + (ray.origin - sphere.center)) = r^2
-      //   = [t^2 * dotself(ray.direction)] + [2 * t * dot(ray.direction, ray.origin - sphere.center)] + [dotself(ray.origin - sphere.center) - (r^2)] = 0
-      //  where 
-      //        ray.origin = A
-      //        ray.direction = B
-      //        ray(t) = A + t * B,
-      //        sphere.center = C
-      //   = [t^2 * dotself(B)] + [2 * t * dot(B, A-C)] + [dotself(A-C) - (r^2)] = 0
-      //   = [t^2 * (a)] + [t * (b)] + [(c)] = 0
-      // just solve quadratic equation (-b +/- sqrt(b^2 - 4ac)) / 2a
-      // we need to check just is answer exists. so, check if (b^2 - 4ac) is positive
-      let sphere = self;
-      let center = ray.origin - sphere.center;
-      let quadratic_a = Matrix::dot(&ray.direction, &ray.direction);
-      let quadratic_b = Matrix::dot(&center,&ray.direction) * 2.0;
-      let quadratic_c = Matrix::dot(&center, &center) - sphere.radius.powi(2);
-      let discriminant = quadratic_b.powi(2) - 4.0 * quadratic_a * quadratic_c;
+      let (A, B, C, r): _ = (&ray.origin, &ray.direction, &self.center, &self.radius);
+      let a = B.dot(B);
+      let b = B.dot(&(A-C));
+      let c = (A-C).dot(&(A-C)) - r.powi(2);
+      let discriminant = b.powi(2) - a * c;
       if discriminant < 0.0 {
           return None
       } else {
-          let t_pos = (-quadratic_b - discriminant.sqrt()) / (2.0 * quadratic_a);
-          let t_neg = (-quadratic_b - discriminant.sqrt()) / (2.0 * quadratic_a);
-          let t = t_pos.max(t_neg);
-          if t <= 0.001 || t >= f32::INFINITY { return None }
-          let hit_surface = ray.point_at_parameter(t);
-          return Some(HitRecordGeometric {
-              t: t,
-              position: hit_surface,
-              normal: (hit_surface - sphere.center).normalize(),
-          })
+          let t_pos = (-b - discriminant.sqrt()) / (a);
+          let t_neg = (-b + discriminant.sqrt()) / (a);
+          for t in vec![t_pos, t_neg] {
+            if t < f32::INFINITY && t > 0.001 {
+              let hit_surface = ray.point_at_parameter(t);
+              return Some(HitRecordGeometric {
+                  t: t,
+                  position: hit_surface,
+                  normal: (hit_surface - C).normalize(),
+              })
+            }
+          }
+          None
       }
   }
 }
